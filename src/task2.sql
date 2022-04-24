844805211d4d783da28dc4b415f3ebafec19dda7
lakamb <lakamb@bk.ru>
2022-04-24 21:06:59 +0300

    Add files via upload

---
+++ src/task2.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE VIEW analysis.orders AS (
+SELECT o.order_id, o.order_ts, o.user_id, o.bonus_payment, o.payment, o.cost, o.bonus_grant, stid.status_id
+FROM production.orders o
+LEFT JOIN (
+	SELECT order_id, status_id
+	FROM (
+		SELECT order_id, status_id, dttm, max(dttm) over(PARTITION BY order_id) max_dttm
+		FROM production.OrderStatusLog) t
+		WHERE dttm = max_dttm) stid -- status_id/Стыд за такой несуразный запрос
+		ON stid.order_id = o.order_id)
