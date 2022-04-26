344cee48cb7d842450645b5f0aa7e2b1f361a3ad
lakamb <lakamb@bk.ru>
2022-04-26 23:08:17 +0300

    Update task2.sql

--- src/task2.sql
+++ src/task2.sql
@@ -5,13 +5,11 @@ FROM production.orders o
 LEFT JOIN (
 	SELECT order_id, status_id
 	FROM (
-		SELECT order_id, status_id, dttm, 
-			--max(dttm) over(PARTITION BY order_id) max_dttm
-			rank() over(PARTITION BY order_id ORDER BY dttm DESC) rank_dttm 
+		SELECT order_id, status_id, dttm,
+			row_number() over(PARTITION BY order_id ORDER BY dttm DESC) rn -- понял) действительно. спасибо!
 		FROM production.OrderStatusLog) t
 		WHERE 1=1
-		  --AND dttm = max_dttm -- только мне не совсем понятно, в чем принципиальная разница
-		  AND rank_dttm = 1) stid -- status_id/Стыд за такой несуразный запрос
+		  AND rn = 1) stid -- status_id/Стыд за такой несуразный запрос
 		ON stid.order_id = o.order_id);
 		
 SELECT * FROM analysis.orders
